16d5b1fb35bf929b9a039a0bef8f689318f45be4
Adelita Esparza <aesparza051@gmail.com>
2019-06-17 16:31:22 -0500

    javascript practice using node.js

---
+++ node.js
@@ -0,0 +1,12 @@
+function loopyThing(num) {
+	if (num > 11) {
+		console.log("Hey! That's a large number!");
+	} else if (num < 10) {
+		console.log("Hey! That's a small number!");
+	} else {
+		console.log("Hey! That's not a number!");
+	}
+}
+loopyThing(12);
+console.log('==========');
+loopyThing(5);
